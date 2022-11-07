7ed39f96259a9987df9f2560efbe3316029488c7
JadmDev <jadmmr.2022@gmail.com>
2022-11-07 15:10:44 -0500

    Initial commit

---
+++ movinfo/src/main/java/net/pirrisdev/movinfo/ui/view/Main/MainActivity.kt
@@ -0,0 +1,46 @@
+package net.pirrisdev.movinfo.ui.view.Main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import net.pirrisdev.movinfo.ui.view.Main.ui.theme.MovinfoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovinfoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Q izo $name!", modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp).alpha(0.5f))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MovinfoTheme {
+        Greeting("Antonio")
+    }
+}
