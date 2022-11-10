cd13ab6333af1bbec6e4bf38eea086904f2c2c3b
JadmDev <jadmmr.2022@gmail.com>
2022-11-10 16:03:17 -0500

    Initial commit

--- movinfo/src/main/java/net/pirrisdev/movinfo/ui/view/Main/MainActivity.kt
+++ movinfo/src/main/java/net/pirrisdev/movinfo/ui/view/Main/MainActivity.kt
@@ -1,18 +1,23 @@
 package net.pirrisdev.movinfo.ui.view.Main
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,36 +27,65 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            Content()
         }
     }
 }
 
-@Preview
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.PIXEL_4,
+    uiMode = UI_MODE_NIGHT_NO
+)
 @Composable
-fun App() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                Color.Magenta
-            ),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.app_icon),
-            contentDescription = "Logo App",
-            modifier = Modifier.width(320.dp).height(320.dp)
+fun Content() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        MovieItem()
+        MovieItem()
+        MovieItem()
+    }
+}
 
-        )
+@Composable
+fun MovieItem() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .width(150.dp)
+            .height(200.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier
+                .weight(1f)
+                .background(color = Color.Red)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.app_icon),
+                contentDescription = "Item Photo",
+                alignment = Alignment.Center,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.fillMaxSize()
+            )
+            Image(
+                painter = painterResource(id = R.drawable.play_circle),
+                contentDescription = "Item Play Button",
+                alignment = Alignment.Center,
+                colorFilter = ColorFilter.tint(Color.White),
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(64.dp)
+            )
+        }
         Text(
-            text = "Jhon Diaz",
-            fontSize = 32.sp,
-            color = Color.White,
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center
+            text = "Title 9",
+            fontSize = 26.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .clickable { /*TODO*/ }
         )
-        Text(text = "Learning")
-        Text(text = "HI!")
     }
 }
